14ba5b8e5cdd24e745f085daa4ee8f727ccd1d16
yan <yan@yan-desktop.acada.ca>
2018-01-25 17:28:46 -0500

    Added more tests and removed some unneccessary ones

--- src/test/scala/com/draugrsoft/integration/helper/cron/test/NextRunCalculatorTest.scala
+++ src/test/scala/com/draugrsoft/integration/helper/cron/test/NextRunCalculatorTest.scala
@@ -7,6 +7,7 @@ import java.util.Calendar
 import com.draugrsoft.integration.helper.messages.CommonActorMessages._
 import scala.concurrent.duration._
 import com.draugrsoft.integration.helper.constants.Cron._
+import com.draugrsoft.integration.helper.constants.TimeConstant._
 
 class NextRunCalculatorTest extends WordSpec with Matchers {
 
@@ -45,8 +46,8 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
         seconds = new SecondsVal(0),
         minutes = new MinutesVal(0),
         hours = new HoursVal(5),
-        dayOfMonth = All,
-        dayOfWeek = All,
+        dayOfMonth = NoVal,
+        dayOfWeek = NoVal,
         month = All)
       val result = NextRunCalculator(twoPM, fivePMTrigger)
       assertResult((fivePM - twoPM).millis)(result)
@@ -55,8 +56,8 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
         seconds = new SecondsVal(0),
         minutes = new MinutesVal(0),
         hours = HoursVal(4 :: 5 :: Nil),
-        dayOfMonth = All,
-        dayOfWeek = All,
+        dayOfMonth = NoVal,
+        dayOfWeek = NoVal,
         month = All)
       val resultTwo = NextRunCalculator(twoPM, fourPMFivePMTrigger)
       assertResult((fourPM - twoPM).millis)(resultTwo)
@@ -66,8 +67,8 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
         seconds = new SecondsVal(0),
         minutes = new MinutesVal(0),
         hours = HoursVal(4 :: 5 :: Nil),
-        dayOfMonth = All,
-        dayOfWeek = All,
+        dayOfMonth = NoVal,
+        dayOfWeek = NoVal,
         month = All)
       val resultThree = NextRunCalculator(twoPM, sixThirtyPMTrigger)
       assertResult((sixThirtyPM - twoPM).millis)(resultThree)
@@ -80,8 +81,15 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
       val nextSecListRollOver = NextRunCalculator.getNextSec(SecondsVal(List(5,25,45)), 55)
       assertResult(5)(nextSecListRollOver)
       
+      // Current time is equal to the last in the list
+      val nextSecListRollOverStartOnTrig = NextRunCalculator.getNextSec(SecondsVal(List(5,25,45)), 45)
+      assertResult(5)(nextSecListRollOverStartOnTrig)
+      
       val nextSecList = NextRunCalculator.getNextSec(SecondsVal(List(5,25,45)), 20)
       assertResult(25)(nextSecList)
+      
+      val nextSecListStartOnTrig = NextRunCalculator.getNextSec(SecondsVal(List(5,25,45)), 5)
+      assertResult(25)(nextSecListStartOnTrig)
       
       // With a wildcard, the next second should be returned
       val nextSecAll = NextRunCalculator.getNextSec(All, 24)
@@ -102,8 +110,15 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
       val nextMinListRollOver = NextRunCalculator.getNextMin(MinutesVal(List(5,25,45)), 55)
       assertResult(5)(nextMinListRollOver)
       
+       // If there is no next item on the list. Start on last value
+      val nextMinListRollOverStartOnTrigl = NextRunCalculator.getNextMin(MinutesVal(List(5,25,45)), 45)
+      assertResult(5)(nextMinListRollOverStartOnTrigl)
+      
       val nextMinList = NextRunCalculator.getNextMin(MinutesVal(List(5,25,45)), 20)
       assertResult(25)(nextMinList)
+      
+      val nextMinListStartOnTrig = NextRunCalculator.getNextMin(MinutesVal(List(5,25,45)), 5)
+      assertResult(25)(nextMinListStartOnTrig)
       
       // With a wildcard, the next minute should be returned
       val nextMinAll = NextRunCalculator.getNextMin(All, 24)
@@ -124,8 +139,15 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
       val nextHourListRollOver = NextRunCalculator.getNextHour(HoursVal(List(4,8,12)), 13)
       assertResult(4)(nextHourListRollOver)
       
+       // If there is no next item on the list. Starting on last trigger val
+      val nextHourListRollOverStartOnTrig = NextRunCalculator.getNextHour(HoursVal(List(4,8,12)), 12)
+      assertResult(4)(nextHourListRollOverStartOnTrig)
+      
       val nextHourList = NextRunCalculator.getNextHour(HoursVal(List(4,8,12)), 6)
       assertResult(8)(nextHourList)
+      
+      val nextHourListStartOnTrig = NextRunCalculator.getNextHour(HoursVal(List(4,8,12)), 4)
+      assertResult(8)(nextHourListStartOnTrig)
       
       // With a wildcard, the next hour should be returned
       val nextHourAll = NextRunCalculator.getNextHour(All, 22)
@@ -143,27 +165,43 @@ class NextRunCalculatorTest extends WordSpec with Matchers {
     "produce the correct next Day of Month value" in {
       
        // If there is no next item on the list
-      val nextDayOfMonthListRollOver = NextRunCalculator.getNextDayOfMonth(DayOfMonthVal(List(1,5,10)), 24)
+      val nextDayOfMonthListRollOver = NextRunCalculator.getNextDayOfMonth(DayOfMonthVal(List(1,5,10)), 11)
       assertResult(Some(1))(nextDayOfMonthListRollOver)
+      
+      // Start on the last item
+      val nextDayOfMonthListRollOverStartOnTrig = NextRunCalculator.getNextDayOfMonth(DayOfMonthVal(List(1,5,10)), 10)
+      assertResult(Some(1))(nextDayOfMonthListRollOverStartOnTrig)
       
       val nextDayOfMonthlist = NextRunCalculator.getNextDayOfMonth(DayOfMonthVal(List(1,5,10)), 3)
       assertResult(Some(5))(nextDayOfMonthlist)
       
-      // With a wildcard, the next day of month should be returned
-      val nextDayOfMonthAll = NextRunCalculator.getNextDayOfMonth(All, 24)
-      assertResult(Some(25))(nextDayOfMonthAll)
+      val nextDayOfMonthlistStartOnTrig = NextRunCalculator.getNextDayOfMonth(DayOfMonthVal(List(1,5,10)), 1)
+      assertResult(Some(5))(nextDayOfMonthlistStartOnTrig)
+      
       
       // With one cron value, it should always return that value
       val nextValSingle = NextRunCalculator.getNextDayOfMonth(new DayOfMonthVal(25), 29)
       assertResult(Some(25))(nextValSingle)
       
-      // 31 should go to 1
-      val nextHourAllRollOver = NextRunCalculator.getNextDayOfMonth(All, 31);
-      assertResult(Some(1))(nextHourAllRollOver)
-      
       // If input is NoVal, output should be None
       val nextHourNoVal = NextRunCalculator.getNextDayOfMonth(NoVal, 3)
       assertResult(None)(nextHourNoVal)
+    }
+    
+    "produce the correct next Day of Week value" in {
+      
+      
+      val nextDayofWeekLeftListRollOver = NextRunCalculator.getNextDayOfWeek(DayOfWeekVal(Left(List(2,3,4))), 5)
+      assertResult(Some(2))(nextDayofWeekLeftListRollOver)
+      
+      val nextDayofWeekLeftListRollOverStartOnTrig = NextRunCalculator.getNextDayOfWeek(DayOfWeekVal(Left(List(2,3,4))), 4)
+      assertResult(Some(2))(nextDayofWeekLeftListRollOverStartOnTrig)
+      
+       val nextDayofWeekRightListRollOver = NextRunCalculator.getNextDayOfWeek(DayOfWeekVal(Right(List(Sun,Mon,Tues,Wed))), 5)
+      assertResult(Some(1))(nextDayofWeekRightListRollOver)
+      
+      val nextDayOfWeekLeftlist = NextRunCalculator.getNextDayOfWeek(DayOfWeekVal(Left(List(2,3,6))), 4)
+      assertResult(Some(6))(nextDayOfWeekLeftlist)
     }
   }
 
